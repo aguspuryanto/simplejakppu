6cce73b5d9a9d22e60c0aae378071ac16245bcfb
Agus Puryanto <aguspuryanto@gmail.com>
2023-07-28 08:59:36 +0700

    update

--- application/views/login.php
+++ application/views/login.php
@@ -1,113 +1,104 @@
+
 <!DOCTYPE html>
-<html>
-  <head>
+<html lang="en">
+
+<head>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminCRUD | Log in</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.6 -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/eksternal/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/eksternal/ionicons.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/iCheck/square/blue.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <style type="text/css">
-      html, body {
-        height: 100%;
-      }
-      .login-page { background-color: #fff; }
-      .login-box { background-color: #d2d6de; height: fit-content; }
-      .content { padding: 7rem 0; }
-      .content .contents, .content .bg { width: 50%; }
-    </style>
-  </head>
-  <body class="hold-transition login-page">
-    <div class="content">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-12 col-sm-6 col-md-8 hidden-xs">
-            <img src="assets/img/undraw_remotely_2j6y.svg" alt="Image" class="img-fluid">
-          </div>
-          <div class="col-xs-12 col-sm-6 col-md-4">
+    <title>SB Admin 2 - Login</title>
 
-            <div class="login-box">
-              <div class="login-logo">
-                <a href="<?php echo base_url(); ?>assets/index2.html"><b>SIMPLE JAK</b>PPU</a>
-              </div>
+    <!-- Custom fonts for this template-->
+    <link href="<?= base_url(); ?>public/themes/sb-admin-2/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
-              <!-- /.login-logo -->
-              <div class="login-box-body">
-                <p class="login-box-msg">
-                  Log in to start your session
-                </p>
+    <!-- Custom styles for this template-->
+    <link href="<?= base_url(); ?>public/themes/sb-admin-2/css/sb-admin-2.min.css" rel="stylesheet">
 
-                <form action="<?php echo base_url('Auth/login'); ?>" method="post">
-                  <div class="form-group has-feedback">
-                    <input type="text" class="form-control" placeholder="auwfar" name="username">
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                  </div>
-                  <div class="form-group has-feedback">
-                    <input type="password" class="form-control" placeholder="auwfar" name="password">
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                  </div>
-                  <div class="row">
-                    <div class="col-xs-12">
-                      <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    </div>
+                                    <form class="user">
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user"
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                placeholder="Enter Email Address...">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="exampleInputPassword" placeholder="Password">
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">Remember
+                                                    Me</label>
+                                            </div>
+                                        </div>
+                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </a>
+                                        <hr>
+                                        <a href="index.html" class="btn btn-google btn-user btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
+                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        </a>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="register.html">Create an Account!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                </form>
+                </div>
 
-                <!-- <div class="social-auth-links text-center">
-                  <p>- OR -</p>
-                  <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-                    Facebook</a>
-                  <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-                    Google+</a>
-                </div> -->
-                <!-- /.social-auth-links -->
-
-                <!-- <a href="#">I forgot my password</a><br>
-                <a href="register.html" class="text-center">Register a new membership</a> -->
-
-              </div>
-              <!-- /.login-box-body -->
-              <?php
-                echo show_err_msg($this->session->flashdata('error_msg'));
-              ?>
             </div>
-            <!-- /.login-box -->
 
-          </div>
         </div>
-      </div>
+
     </div>
 
-    <!-- jQuery 2.2.3 -->
-    <script src="<?php echo base_url(); ?>assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
-    <!-- Bootstrap 3.3.6 -->
-    <script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
-    <!-- iCheck -->
-    <!-- <script src="<?php echo base_url(); ?>assets/plugins/iCheck/icheck.min.js"></script> -->
-    <!-- <script>
-      $(function () {
-        $('input').iCheck({
-          checkboxClass: 'icheckbox_square-blue',
-          radioClass: 'iradio_square-blue',
-          increaseArea: '20%' // optional
-        });
-      });
-    </script> -->
-  </body>
+    <!-- Bootstrap core JavaScript-->
+    <script src="<?= base_url(); ?>public/themes/sb-admin-2/vendor/jquery/jquery.min.js"></script>
+    <script src="<?= base_url(); ?>public/themes/sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="<?= base_url(); ?>public/themes/sb-admin-2/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="<?= base_url(); ?>public/themes/sb-admin-2/js/sb-admin-2.min.js"></script>
+
+</body>
+
 </html>
