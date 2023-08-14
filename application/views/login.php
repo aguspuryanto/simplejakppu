067753285fd4f69d65b22d99ebb94a2067c885a7
Agus Puryanto <aguspuryanto@gmail.com>
2023-08-14 20:36:08 +0700

    update

--- application/views/login.php
+++ application/views/login.php
@@ -10,7 +10,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Login</title>
+    <title>SIMPLEJAK PPU - Login</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url(); ?>public/themes/sb-admin-2/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
