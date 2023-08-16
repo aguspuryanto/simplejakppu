a2cf6e35eaf640282dea35da194404daea46b813
Agus Puryanto <aguspuryanto@gmail.com>
2023-08-16 13:45:42 +0700

    update

---
+++ application/migrations/027_add_bmnkelola.php
@@ -0,0 +1,73 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed'); 
+
+class Migration_add_bmnkelola extends CI_Migration { 
+    public $table_name = 'epak_bmnkelola';
+
+    public function up() {
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'kelompok' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'kode_barang' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'nama_barang' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'nup' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'kondisi' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'merk_tipe' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'tgl_perolehan' => array(
+                'type' => 'timestamp'
+            ),
+            'nilai_perolehan' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'kuantiti' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'status_kelola' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'no_psp' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'tgl_psp' => array(
+                'type' => 'timestamp'
+            ),
+            'nobpkb' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'nopol' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'pemakai' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'jml_kib' => array(
+                'type' => 'VARCHAR', 'constraint' => '100'
+            ),
+            'created_at' => array('type' => 'timestamp')
+        ));
+
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table($this->table_name, TRUE);
+    }
+
+    public function down() { 
+        $this->dbforge->drop_table($this->table_name, TRUE);
+    }
+}
