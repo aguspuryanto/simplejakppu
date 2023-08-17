38818a3dd461550310ad9b606fa7e38e5904a68a
Agus Puryanto <aguspuryanto@gmail.com>
2023-08-17 12:22:15 +0700

    update

--- application/migrations/028_update_perkara.php
+++ application/migrations/028_update_perkara.php
@@ -17,5 +17,12 @@ class Migration_update_perkara extends CI_Migration {
                 'kajari_note' => array('type' => 'VARCHAR', 'constraint' => 100, 'after' => 'keterangan'))
             );
         }
+
+        $table_name = 'epak_inkracth';
+        if (!$this->db->field_exists('kajari_note', $table_name)) {
+            $this->dbforge->add_column($table_name, array(
+                'kajari_note' => array('type' => 'VARCHAR', 'constraint' => 100, 'after' => 'keterangan'))
+            );
+        }
     }
 }
--- application/views/pidum/_list_inkracth.php
+++ application/views/pidum/_list_inkracth.php
@@ -45,6 +45,36 @@
 
 <?php include_once('_modal_inkracth.php'); ?>
 
+<!-- Modal -->
+<div id="myModalNote" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Catatan Kajari</h4>
+      </div>
+      <div class="modal-body">
+        <?=form_open('', array('id' => 'formNote', 'role' => 'form'));?>
+
+            <div class="form-group">
+                <!-- <label>Catatan Kajari</label> -->
+                <?=form_textarea('kajari_note', '', array('class' => 'form-control', 'id' => 'input-kajari_note', 'rows' => '4', 'cols' => '40'));?>
+                <div id="error"></div>
+            </div>
+
+            <?=form_hidden('id', ''); ?>
+
+            <button type="submit" class="btn btn-primary" id="formNote">Simpan Data</button>
+            <button type="reset" class="btn btn-default">Kosongkan Data</button>
+        <?=form_close();?>
+      </div>
+    </div>
+
+  </div>
+</div>
+
 <script type="text/javascript">
 $( document ).ready(function() {
     $.fn.datepicker.defaults.format = "dd/mm/yyyy";
