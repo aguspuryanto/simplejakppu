84cccbcbf5cc05195ea1fbff586c45f7621268a5
Agus Puryanto <aguspuryanto@gmail.com>
2023-08-17 10:07:44 +0700

    update

--- application/views/pidsus/mafia_pelabuhan.php
+++ application/views/pidsus/mafia_pelabuhan.php
@@ -6,14 +6,14 @@
   <div class="panel-body m0">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h4 class="pull-left"><?=@$judul; ?></h4>
+                <h4 class="pull-left">DATA <?=@$judul; ?></h4>
                 <div class="pull-right">
                     <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModalPnbp"><i class="glyphicon glyphicon-plus"></i> Tambah Data</button>
                 </div>
                 <div class="clearfix"></div>
             </div>
             <div class="panel-body">
-                <?php include_once('_list_pnbp.php'); ?>
+                <?php include_once('_list_mafia.php'); ?>
             </div>
         </div>
   </div>
