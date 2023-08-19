22c014f3c2758e4d05d36ed82971c6cd606b7fc1
Agus Puryanto <aguspuryanto@gmail.com>
2023-08-19 19:36:26 +0700

    update

--- application/migrations/029_add_datun_menu.php
+++ application/migrations/029_add_datun_menu.php
@@ -28,6 +28,32 @@ class Migration_add_datun_menu extends CI_Migration {
 
         $this->dbforge->add_key('id', TRUE);
         $this->dbforge->create_table($this->table_name, TRUE);
+
+
+        $menu_datun = array(array(
+            'nama' => "gakkum",
+            'deskripsi' => "GAKKUM",
+            'parent' => 0,
+        ), array(
+            'nama' => "timkum",
+            'deskripsi' => "TIMKUM",
+            'parent' => 0,
+        ), array(
+            'nama' => "bankum",
+            'deskripsi' => "BANKUM",
+            'parent' => 0,
+        ), array(
+            'nama' => "thl",
+            'deskripsi' => "THL",
+            'parent' => 0,
+        ), array(
+            'nama' => "yankum",
+            'deskripsi' => "YANKUM",
+            'parent' => 0,
+        ));
+
+        // $menu_datun = array_merge($menu_datun, []);
+        $this->db->insert_batch($this->table_name, $menu_datun);
     }
 
     public function down()
