7e92b70859a27d335e2cd9d8462b2f92351a14db
Agus Puryanto <aguspuryanto@gmail.com>
2023-08-26 10:42:53 +0700

    update

--- application/views/datun/_penyelamatan_kn.php
+++ application/views/datun/_penyelamatan_kn.php
@@ -26,7 +26,9 @@
 
 <script>
 $(document).ready(function () {
-    $('#example1').DataTable();
+    var table = $('#example1').DataTable();
+     
+    $('#error').html(" ");
 
     $('#form').find('select#kategori').on('change', function(e) {
         e.preventDefault();
@@ -41,6 +43,117 @@ $(document).ready(function () {
                 $('#kegiatan').find('option').remove().end().append(data);
             });
         }
+    });
+
+    $('#form-submit').on('click', function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            type: "POST",
+            url: "<?=$Urladd;?>", 
+            data: $("#form").serialize(),
+            dataType: "json",
+            beforeSend : function(xhr, opts){
+                $('#form-submit').text('Loading...').prop("disabled", true);
+            },
+            success: function(data){
+                console.log(data, "data");
+                if(data.success == true){
+                    setTimeout(function(){
+                        window.location.reload();
+                    }, 3000);
+                } else {
+                    $.each(data, function(key, value) {
+                        $('#input-' + key).addClass('is-invalid');
+                        $('#input-' + key).parents('.form-group').find('#error').html(value);
+                    });
+                }
+            }
+        });
+    });
+
+    $('#form input').on('keyup', function () { 
+        $(this).removeClass('is-invalid').addClass('is-valid');
+        $(this).parents('.form-group').find('#error').html(" ");
+    });
+
+    $(document).on('click', '.btnAdd', function (e){
+        e.preventDefault();
+        $('#form')[0].reset();
+    });
+
+    $(document).on('click', '.btnNote', function (e){
+        e.preventDefault();
+        var dataId = $(this).attr("data-id");
+        // console.log(dataId, '_dataId');
+        $('#formNote input[name=id]').val(dataId);
+
+        $.get("<?=$Urldetail;?>/" + dataId, function(data, status){
+            console.log(data.data, "data");
+            $('#formNote').find('#input-kajari_note').val(data.data.kajari_note);
+        });        
+    });
+
+    $('#formNote').submit(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            type: "POST",
+            url: "<?=$Urlnote;?>", 
+            data: $("#formNote").serialize(),
+            dataType: "json",  
+            beforeSend : function(xhr, opts){
+                $('#formNote').text('Loading...').prop("disabled", true);
+            },
+            success: function(data){
+                console.log(data, "data");
+                if(data.success) {
+                    $('#myModalNote').modal('hide'); 
+                    setTimeout(function(){
+                        window.location.reload();
+                    }, 1000);
+                }
+            }
+        });
+    });
+
+    $(document).on('click', '.btnEdit', function (e) {
+        e.preventDefault();
+        var dataId = $(this).attr("data-id");
+        console.log(dataId, '_dataId');
+
+        $('#form input[name=id]').val(dataId);
+
+        let promise = new Promise(function(resolve, reject) {
+            // resolve(dataId);
+        });
+
+        $.get("<?=$Urldetail;?>/" + dataId, function(data, status){
+            console.log(data, "data");
+            $.each(data.data, function(key, value) {
+                if(key == 'kategori') {
+                    $('#kategori').val(value).change();
+                } else {
+                    $('#input-' + key).val(value);
+                }
+            });
+
+            $('#form input[name=kegiatan]').val(data.data.kegiatan);
+        });
+    });
+
+    $(document).on('click', '.btnRemove', function (e) {
+        e.preventDefault();
+        var dataId = $(this).attr("data-id");
+        console.log(dataId, '_dataId');
+
+        if (confirm("Apakah anda yakin ingin menghapus data ini?")==true){
+            // $(this).closest("tr").remove();
+            table.row( $(this).parents('tr') ).remove().draw();
+            $.post("<?=$Urlremove;?>/", {id: dataId}, function(result){
+                console.log(result, "_result");
+            });
+        };
     });
 });
 </script>
